9f98e525cf3ff048c691a28fdc45d4820f73395b
Andrii <andriishtaher@gmail.com>
2025-10-22 20:50:02 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+
+const largeImg = document.querySelector('.gallery__large-img');
+
+const allImgs = document.querySelectorAll('.list-item__link');
+
+allImgs.forEach((img) => {
+  img.addEventListener('click', (currEvent) => {
+    currEvent.preventDefault();
+
+    const currentA = currEvent.currentTarget;
+    const currentImg = currentA.querySelector('img');
+
+    largeImg.src = currentA.href;
+    largeImg.alt = `main image ${currentImg.alt}`;
+  });
+});
